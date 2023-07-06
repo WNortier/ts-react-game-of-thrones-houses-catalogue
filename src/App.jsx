9953ad7120d94a7d4f20ff9644bee244af5f729b
wnortier <warwick.nortier@gmail.com>
2023-07-06 23:31:15 +0200

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,7 @@ function App() {
       element: <BasicForm init={init} setVideoComplete={setVideoComplete} videoComplete={videoComplete} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />,
       // action: newsletterAction,
     }
-  ], { basename: import.meta.env.DEV ? '/' : '/' })
+  ], { basename: import.meta.env.DEV ? '/' : '/game-of-thrones-houses-catalogue/' })
 
   return (
 
