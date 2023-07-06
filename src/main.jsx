dbe9e6f95845b7d07607ef261f6852c01ada4db7
wnortier <warwick.nortier@gmail.com>
2023-07-06 23:42:23 +0200

    debugging

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import '/styles/index.css'
-// import '/styles/pages/splash.css'
-// import '/styles/animations.css'
-// import '/styles/media-queries.css'
-// import '/styles/layout.css'
-// import '/styles/interactive.css'
-// import '/styles/forms.css'
+import './assets/styles/index.css'
+import './assets/styles/pages/splash.css'
+import './assets/styles/animations.css'
+import './assets/styles/media-queries.css'
+import './assets/styles/layout.css'
+import './assets/styles/interactive.css'
+import './assets/styles/forms.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
