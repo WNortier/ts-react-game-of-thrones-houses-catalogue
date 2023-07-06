ed3aa6596c63e30d652be7af56eefa3f590c9fe3
wnortier <warwick.nortier@gmail.com>
2023-07-06 18:38:33 +0200

    pushing code

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import './styles/index.css'
+import './styles/pages/splash.css'
 import './styles/animations.css'
 import './styles/media-queries.css'
+import './styles/layout.css'
+import './styles/interactive.css'
+import './styles/forms.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
