c58adae6ada5cd1f36ee61ec82dfb9f2b222ac08
wnortier <warwick.nortier@gmail.com>
2023-07-07 00:42:43 +0200

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Splash from './components/Splash';
 
 function App() {
   const [count, setCount] = useState(0)
-  const [bg, setBg] = useState("/mainbg01.jpeg")
+  const [bg, setBg] = useState("./assets/mainbg01.jpeg")
   const [isLoggedIn, setIsLoggedIn] = useState(true)
   const [videoComplete, setVideoComplete] = useState(false)
   const [init, setInit] = useState(false)
@@ -36,12 +36,12 @@ function App() {
     }
     setInterval(() => {
       switch (bg) {
-        case "mainbg01.jpeg": {
-          setBg("mainbg01.jpeg")
+        case "./assets/mainbg01.jpeg": {
+          setBg("./assets/mainbg01.jpeg")
           break
         }
-        case "mainbg01.jpeg": {
-          setBg("mainbg01.jpeg")
+        case "./assets/mainbg01.jpeg": {
+          setBg("./assets/mainbg01.jpeg")
           break
         }
       }
@@ -110,10 +110,10 @@ function App() {
 
       <RouterProvider router={router} />
       <audio id='music'>
-        <source src="/Enchanted Forest Music & Mystical Vocals ✦ Ethereal Fantasy Music ✦ 528 hz ✦ Path To The Elven Lands.mp3" type="audio/mpeg"></source>
+        <source src="./assets/Enchanted Forest Music & Mystical Vocals ✦ Ethereal Fantasy Music ✦ 528 hz ✦ Path To The Elven Lands.mp3" type="audio/mpeg"></source>
       </audio>
       <video muted id='video' loop style={{ position: "absolute", top: "0", left: "0", bottom: "0", right: "0", width: "100%", height: "95%", margin: 'auto' }}>
-        <source src="/got.mp4" type="video/mp4"></source>
+        <source src="./assets/got.mp4" type="video/mp4"></source>
       </video>
     </Container>
   )
